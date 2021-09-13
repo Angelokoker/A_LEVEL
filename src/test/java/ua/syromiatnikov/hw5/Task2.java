636359eb4d3cf16bac12779f1f5ad285d2bf3dd5
Angelokoker <angelokoker@gmail.com>
2021-09-13 13:34:05 +0300

    first commit

---
+++ src/test/java/ua/syromiatnikov/hw5/Task2.java
@@ -0,0 +1,12 @@
+package ua.syromiatnikov.hw5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task2Test {
+
+    @Test
+    void sortingCheck() {
+    }
+}
